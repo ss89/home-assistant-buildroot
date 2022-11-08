7287423809c68c688bb02c83889b70482d31e3bb
Sebastian Struß <sebastian.struss@gmail.com>
2022-11-08 19:55:43 +0100

    added odroid-u3 support - WIP

--- board/hardkernel/odroidu3/post-image.sh
+++ board/hardkernel/odroidu3/post-image.sh
@@ -6,12 +6,12 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 cp ${BOARD_DIR}/boot.ini ${BINARIES_DIR}/
 
-# The bl1.bin.hardkernel file provided by the uboot hardkernel repository is overwritten
-# by the bl2.bin.hardkernel in the sd_fusing.sh script because it is too big.
+# The bl1.HardKernel file provided by the uboot hardkernel repository is overwritten
+# by the bl2.HardKernel in the sd_fusing.sh script because it is too big.
 # In order to implement this in genimage, we need to truncate the bl1.bin file
 # so that it does not exceed the available place.
 # An issue has been filled about this: https://github.com/hardkernel/u-boot/issues/45
-truncate -s 15360 ${BINARIES_DIR}/bl1.bin.hardkernel
+truncate -s 15360 ${BINARIES_DIR}/bl1.HardKernel
 
 rm -rf "${GENIMAGE_TMP}"
 
